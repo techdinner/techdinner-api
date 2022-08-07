9e65db565d75f5bf7ccc6c3edb1ea271139290ee
matheusdearaujo <matheusouzadearaujo@gmail.com>
2022-08-07 01:01:20 -0300

    Users

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -3,7 +3,15 @@ import { Request, Response } from "express";
 export class UserController {
 	async getAll(req: Request, res: Response) {
 		try {
-			return res.status(200).json();
+			const data = [
+				{
+					name: "Matheus",
+				},
+				{
+					name: "Thony",
+				},
+			];
+			return res.status(200).json(data);
 		} catch (error) {
 			console.log(error);
 			return res.status(500).json({ message: "Internal Sever Error" });
