6bffb8925242fd1f37cd786e7a650b6b11c6e02f
matheusdearaujo <matheusouzadearaujo@gmail.com>
2022-09-02 14:11:58 -0300

    fix: refactoring

--- src/app/entities/UserEntity.ts
+++ src/app/entities/UserEntity.ts
@@ -1,18 +1,24 @@
 import { BaseEntity } from "./BaseEntity";
 
+interface UserEntityProps {
+	id?: string;
+	name: string;
+	email: string;
+	password: string;
+	active: boolean;
+	role: number;
+	created_at?: Date;
+	updated_at?: Date;
+}
+
 export class UserEntity extends BaseEntity {
-	constructor(
-		public name: string,
-		public email: string,
-		public password: string,
-		public active?: boolean,
-		public role?: number,
-		public created_at?: Date,
-		public updated_at?: Date,
-		id?: string,
-	) {
-		super(id);
-		this.active = active ?? true;
-		this.role = role ?? 3;
+	constructor(private props: UserEntityProps) {
+		super(props.id);
+		props.active = props.active ?? true;
+		props.role = props.role ?? 3;
+	}
+
+	get active(): boolean {
+		return this.props.active;
 	}
 }
