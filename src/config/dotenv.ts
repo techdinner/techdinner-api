80bc9cbbfe20b4df62df94232010c944a15c71e2
matheusdearaujo <matheusouzadearaujo@gmail.com>
2022-09-14 18:14:25 -0300

    configs

--- src/config/dotenv.ts
+++ src/config/dotenv.ts
@@ -8,7 +8,7 @@ config({
 export const environments = {
 	Port: process.env.APP_PORT as unknown as number | 5000,
 	DBHost: process.env.DB_HOST,
-	DBPort: process.env.DB_PORT,
+	DBPort: process.env.DB_PORT as number | undefined,
 	DBUser: process.env.DB_USER,
 	DBPass: process.env.DB_PASS,
 	DBName: process.env.DB_NAME,
