bf07d11ecbd518181d1d7956503903fd260d2c88
Matheus Souza <matheusouzadearaujo@gmail.com>
2022-12-12 17:34:34 -0300

    feat: add salt size number in private variable

--- src/infra/bcrypt/BcryptHashAdapter.ts
+++ src/infra/bcrypt/BcryptHashAdapter.ts
@@ -2,7 +2,9 @@ import { hash } from "bcryptjs";
 import { HashRepository } from "@/app/repositories/crypt/HashRepository";
 
 export class BcryptHashAdapter implements HashRepository {
+  private readonly _saltSize: number = 12;
+
   async hash(string: string): Promise<string> {
-    return await hash(string, 12);
+    return await hash(string, this._saltSize);
   }
 }
