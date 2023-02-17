31584d2bea44d3cbbc18874b4cac3836d2070f23
matheusdearaujo <matheusouzadearaujo@gmail.com>
2023-02-16 21:50:31 -0300

    feat: update user request

---
+++ src/infra/http/requests/users/update-user.request.ts
@@ -0,0 +1,17 @@
+import { make } from "simple-body-validator";
+import { HttpError } from "@/app/helpers/http-error";
+import { UpdateUserDTO } from "@/app/dtos/users/update-user.dto";
+
+export const validate = (data: UpdateUserDTO): void => {
+  const rules = {
+    name: ["required"],
+    cpf: ["required"],
+    phone: ["required"],
+  };
+
+  const validator = make().setData(data).setRules(rules);
+
+  if (!validator.stopOnFirstFailure().validate()) {
+    throw new HttpError(validator.errors().first(), 422);
+  }
+};
