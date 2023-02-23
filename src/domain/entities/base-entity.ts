eba07a84105c6b8ea8c0cc3b5377cd11b1045b2a
matheusdearaujo <matheusouzadearaujo@gmail.com>
2023-02-23 14:19:14 -0300

    chore: remove uuid lib

--- src/domain/entities/base-entity.ts
+++ src/domain/entities/base-entity.ts
@@ -1,9 +1,9 @@
-import { v4 as uuid } from "uuid";
+import { randomUUID } from "node:crypto";
 
 export abstract class BaseEntity {
   public readonly id?: string;
 
   constructor(id?: string) {
-    this.id = id ?? uuid();
+    this.id = id ?? randomUUID();
   }
 }
